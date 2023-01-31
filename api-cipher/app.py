453d6a26fdceab5e881fb99afc219fb59f396dd9
hafidznrg <hafidznur.hn@gmail.com>
2023-01-31 14:07:09 +0700

    feat: add api for encrypt and decrypt playfair cipher

--- api-cipher/app.py
+++ api-cipher/app.py
@@ -1,4 +1,5 @@
 from algorithm.affineCipher import affineDecrypt, affineEncrypt
+from algorithm.playfairCipher import playfairDecrypt, playfairEncrypt
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -26,5 +27,26 @@ def decrypt_affine():
   b = input_json['b']
 
   plaintext = affineDecrypt(ciphertext, m, b)
+
+  return jsonify({"plaintext": plaintext})
+
+
+@app.route('/encrypt/playfair', methods=["POST"])
+def encrypt_playfair():
+  input_json = request.get_json(force=True)
+  plaintext = input_json['plaintext']
+  key = input_json['key']
+
+  ciphertext = playfairEncrypt(plaintext, key)
+
+  return jsonify({"ciphertext": ciphertext})
+
+@app.route('/decrypt/playfair', methods=["POST"])
+def decrypt_playfair():
+  input_json = request.get_json(force=True)
+  ciphertext = input_json['ciphertext']
+  key = input_json['key']
+
+  plaintext = playfairDecrypt(ciphertext, key)
 
   return jsonify({"plaintext": plaintext})
